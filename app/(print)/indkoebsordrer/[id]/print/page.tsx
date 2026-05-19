21879438079ea57f0fcd0e5e40d122d30c5e9520
Jesper <jeod@stofanet.dk>
2026-05-19 15:20:36 +0200

    Fix: fjern konfliktorende (print) route group

--- app/(print)/indkoebsordrer/[id]/print/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-export default async function P({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params
-  redirect(`/po-print/${id}`)
-}
--- app/(print)/indkoebsordrer/[id]/print/print-trigger.tsx
+++
@@ -1,1 +0,0 @@
-export function PrintTrigger() { return null }
